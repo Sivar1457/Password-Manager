ed48db8482d7b1754aef17a028d404ba0cdce425
Sivar1457 <sivashankaran410@gmail.com>
2025-04-23 19:05:32 +0530

    April 23rd changes and updates

--- src/main/webapp/MainPage/inputPageScript.js
+++ src/main/webapp/MainPage/inputPageScript.js
@@ -9,3 +9,11 @@ function togglePassword() {
     toggleIcon.classList.toggle('fa-eye-slash');
 }
 $('toggle-password').addEventListener('click', togglePassword);
+
+inputPage.querySelector('.close-btn').addEventListener('click', function () {
+    inputPage.querySelector('.input-name').value = '';
+    inputPage.querySelector('.input-username').value = '';
+    inputPage.querySelector('.input-password').value = '';
+    inputPage.querySelector('.input-url').value = '';
+    inputPage.classList.remove('show');
+})
--- src/main/webapp/MainPage/mainRightScript.js
+++ src/main/webapp/MainPage/mainRightScript.js
@@ -1,3 +1,45 @@
-$('new-pass-btn').addEventListener('click', function () {
-    $('input-page').classList.toggle('show');
+let passwordCollector = [
+    {
+        id: 1,
+        name : 'Vault',
+        date : 'April 21 , 2025',
+        userName : 'Siva',
+        password : '**********',
+        url : 'https://vault.zoho.com/',
+        description : 'This is a test description for the password manager application. It is used to test the functionality of the application.'
+    },
+    {
+        id: 2,
+        name : 'Drive',
+        date : 'April 25 , 2025',
+        userName : 'Siva',
+        password : '**********',
+        url : 'https://drive.google.com/',
+        description : 'This is a test description for the password manager application. It is used to test the functionality of the application.'
+    }
+]
+
+let rightBar = $('right-bar');
+let inputPage = $('input-page');
+let passUpPage = document.querySelector('.pass-show-up-page');
+
+rightBar.querySelector('.new-pass-btn').addEventListener('click', function () {
+    if ( !inputPage.classList.contains('show') ) {
+        inputPage.classList.add('show');
+    }
+});
+
+
+
+let pass = rightBar.querySelectorAll('.password-page .pass');
+for (let i = 0; i < pass.length; i++) {
+    pass[i].addEventListener('click', function () {
+        if ( !passUpPage.classList.contains('show') ) {
+            $('pass-show-up-page').classList.add('show');
+        }
+    })
+}
+
+passUpPage.querySelector('.close-btn').addEventListener('click', function () {
+        passUpPage.classList.remove('show');
 })
